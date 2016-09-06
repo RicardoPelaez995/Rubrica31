b334f16a043eb40b935246d8d3fa1762504df08e
RicardoPelaez995 <ricardo_pelaez@hotmail.es>
2016-09-05 21:26:28 -0500

    changes
    changesdx

--- src/rurica31/Car.java
+++ src/rurica31/Car.java
@@ -6,5 +6,7 @@ public class Car {
      public static void main(String[] args) {
          System.out.println("Aqui hay una clase publica car");
          System.out.println("HOLAaA!");
+         System.out.println("Car+men = transformer");
+         
      }
 }
