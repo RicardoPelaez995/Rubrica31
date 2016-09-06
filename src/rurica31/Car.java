fe978c6a9c37d6fb5de3e0207bf903f7415dba5f
RicardoPelaez995 <ricardo_pelaez@hotmail.es>
2016-09-05 21:17:31 -0500

    change
    CAMBIO

--- src/rurica31/Car.java
+++ src/rurica31/Car.java
@@ -5,5 +5,6 @@ package rurica31;
 public class Car {
      public static void main(String[] args) {
          System.out.println("Aqui hay una clase publica car");
+         System.out.println("HOLAaA!");
      }
 }
